5450762ea61f05b58316095eefa9b4a92c45291f
Daniel Brahneborg <basic70@hey.com>
2024-06-05 14:24:49 +0000

    avl_data.c: set variables in the declaration

--- avl_data.c
+++ avl_data.c
@@ -10,10 +10,8 @@
 
 mydata *makedata(int key)
 {
-	mydata *p;
-
-	p = (mydata *) malloc(sizeof(mydata));
-	if (p != NULL)
+	mydata* p = (mydata *) malloc(sizeof(mydata));
+	if (p)
 		p->key = key;
 
 	return p;
@@ -21,47 +19,38 @@ mydata *makedata(int key)
 
 int compare_func(const void *d1, const void *d2)
 {
-	mydata *p1, *p2;
-	
-	assert(d1 != NULL);
-	assert(d2 != NULL);
-	
-	p1 = (mydata *) d1;
-	p2 = (mydata *) d2;
+	assert(d1);
+	assert(d2);
+
+	mydata* p1 = (mydata *) d1;
+	mydata* p2 = (mydata *) d2;
 	if (p1->key == p2->key)
 		return 0;
-	else if (p1->key > p2->key)
+	if (p1->key > p2->key)
 		return 1;
-	else
-		return -1;
+	return -1;
 }
 
 void destroy_func(void *d)
 {
-	mydata *p;
-	
-	assert(d != NULL);
-	
-	p = (mydata *) d;
-	free(p);
+	assert(d);
+
+	free(d);
 }
 
 void print_func(void *d)
 {
-	mydata *p;
-	
-	assert(d != NULL);
-	
-	p = (mydata *) d;
+	assert(d);
+
+	mydata* p = (mydata *) d;
 	printf("%d", p->key);
 }
 
 void print_char_func(void *d)
 {
-	mydata *p;
-	
-	assert(d != NULL);
-	
-	p = (mydata *) d;
+	assert(d);
+
+	mydata* p = (mydata *) d;
 	printf("%c", p->key & 127);
 }
+
